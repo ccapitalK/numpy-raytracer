9790d76609fb8dc41d45411c58e5092d2f502144
Sahan Fernando <sahan.h.fernando@gmail.com>
2023-09-17 18:36:27 +1000

    Add ground object + norm method

--- objects.py
+++ objects.py
@@ -1,7 +1,18 @@
 from imports import *
+from vec import *
 
 class Sphere:
     def __init__(self, position, radius, material):
         self.position = np.array(position)
         self.radius = radius
         self.material = material
+
+    def get_norm(self, pos):
+        return normalize(pos - self.position)
+
+class Ground:
+    def __init__(self, material):
+        self.material = material
+
+    def get_norm(self, pos):
+        return np.array((0, 0, 1))

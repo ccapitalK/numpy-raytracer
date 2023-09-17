53747efc11992fce3c71cc9ca66e0460f6383d78
Sahan Fernando <sahan.h.fernando@gmail.com>
2023-09-17 18:37:12 +1000

    Add more props to material

--- material.py
+++ material.py
@@ -1,9 +1,14 @@
 from imports import *
 
 class Material:
-    def __init__(self, albedo, diffuse=0, specular=0, reflect=0, transparency=0):
+    def __init__(self, albedo, diffuse=0, ambient=0.5, specular=0, reflect=0, transparency=0):
         self.albedo = np.array(albedo)
         self.diffuse = diffuse
         self.specular = specular
+        self.ambient = specular
         self.reflect = reflect
         self.transparency = transparency
+
+    def get_color(self, diff, spec):
+        light = diff * self.diffuse + spec * self.specular + self.ambient
+        return light * self.albedo
